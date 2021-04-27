85953ac0456c416124142261e2f4497fbc1ce30f
oleg_nexxxt <nexxxt.07@gmail.com>
2021-04-27 15:08:30 +0300

    27.04

--- js/main.js
+++ js/main.js
@@ -360,3 +360,9 @@ if (document.getElementById("mobCatalogOpen")) {
     }
 }
 
+function getFileName () {
+
+    var file = document.getElementById ('uploaded-file').value;
+    file = file.replace(/\\/g, "/").split('/').pop();
+    document.getElementById ('file-name').innerHTML = 'Имя файла: ' + file;
+}
